2201cd0e6b4ecf15b3b0415f814d3dd95f972fa6
Mohd Shadab <shadab_ibrahim@outlook.com>
2024-06-30 21:52:07 +0530

    submitting code-Sun Jun 30 2024 21:52:06 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -5,29 +5,30 @@ import './../styles/App.css';
 const App = () => {
   const [data, setData] = useState(null);
 
-  // useEffect(() => {
-  //   fetch('https://dummyjson.com/products')
-  //     .then((response) => response.json())
-  //     .then((data) => setData(data))
-  //     .catch((error) => console.error('Error fetching data:', error));
-  // }, []);
-  useEffect(()=>{
-    try{
-      (async function fetchData(){
-        const response =await fetch('https://dummyjson.com/products');
-        const data = await response.json();
-        setData(data);
-      })();
-     
-    }catch(error){
-      alert(error.message)
-    }
-   
-  },[])
+  useEffect(() => {
+    fetch('https://dummyjson.com/products')
+      .then((response) => response.json())
+      .then((data) => setData(data))
+      .catch((error) => console.error('Error fetching data:', error));
+  }, []);
+
+  // useEffect(()=>{
+  //   try{
+  //     (async function fetchData(){
+  //       const response =await fetch('https://dummyjson.com/products');
+  //       const data = await response.json();
+  //       setData(data);
+  //     })();
+
+  //   }catch(error){
+  //     alert(error.message)
+  //   }
+
+  // },[])
 
   return (
     <div>
-      <h1>Data Fetched frm API</h1>
+      <h1>Data Fetched from API</h1>
       {data ? (
         <pre>{JSON.stringify(data, null, 2)}</pre>
       ) : (
