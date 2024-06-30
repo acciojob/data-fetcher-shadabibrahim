3af1e6cbed1f1e1753f5c431128ffca3711047fe
Mohd Shadab <shadab_ibrahim@outlook.com>
2024-06-30 21:48:52 +0530

    submitting code-Sun Jun 30 2024 21:48:52 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,40 @@
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [data, setData] = useState(null);
+
+  // useEffect(() => {
+  //   fetch('https://dummyjson.com/products')
+  //     .then((response) => response.json())
+  //     .then((data) => setData(data))
+  //     .catch((error) => console.error('Error fetching data:', error));
+  // }, []);
+  useEffect(()=>{
+    try{
+      (async function fetchData(){
+        const response =await fetch('https://dummyjson.com/products');
+        const data = await response.json();
+        setData(data);
+      })();
+     
+    }catch(error){
+      alert(error.message)
+    }
+   
+  },[])
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Data Fetched frm API</h1>
+      {data ? (
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
-  )
+  );
 }
 
 export default App
